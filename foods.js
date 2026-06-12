d383774bb101b916a748ab582028092d0512e735
Anju1821 <nanjalidevi3@gmail.com>
2026-06-12 16:49:19 +0530

    Add files via upload

---
+++ foods.js
@@ -0,0 +1,19 @@
+const foods = [
+  {
+    id: 1,
+    name: "Pizza",
+    price: 199
+  },
+  {
+    id: 2,
+    name: "Burger",
+    price: 99
+  },
+  {
+    id: 3,
+    name: "Pasta",
+    price: 149
+  }
+];
+
+export default foods;
